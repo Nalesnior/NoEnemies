caaa7ece9fefb3d56c7c5238faa3aee0518ad0d5
Naleśnior <lisquxdd@gmail.com>
2026-07-04 14:54:53 +0200

    Add project files.

---
+++ NoEnemies/EventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using HarmonyLib;
+using BepInEx;
+
+namespace NoEnemies
+{
+    [HarmonyPatch(typeof(EnemyDirector), "Update")]
+    public static class main
+    {
+        [HarmonyPrefix]
+        public static bool RejectUpdate()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(LevelGenerator), "EnemySpawn")]
+    public static class rejectSpawn
+    {
+        [HarmonyPrefix]
+        public static bool RejectEnemySpawn()
+        {
+            return false;
+        }
+    }
+}
---
+++ NoEnemies/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+
+namespace NoEnemies
+{
+    [BepInPlugin("com.nalesnior.noenemies", "No Enemies", "1.0.0")]
+    public class core : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            var harmony = new Harmony("com.nalesnior.noenemies");
+            harmony.PatchAll();
+        }
+    }
+}
